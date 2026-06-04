fe6042564711eb424de9c012d325d74a945c42dc
yt4dfzrtc4-blip <yt4dfzrtc4@privaterelay.appleid.com>
2026-06-05 00:02:34 +0200

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
